5d97a30f8efe9fd8c40e1702afc5409e48375568
Akash-Bharambe <aakashbharambe100@gmail.com>
2024-08-17 09:35:09 +0530

    minor bug fixes

--- frontend/src/components/Home/Listing.jsx
+++ frontend/src/components/Home/Listing.jsx
@@ -21,7 +21,7 @@ function Listing() {
           </div>
         </div>
         <div className="w-full flex justify-center mt-4">
-          <Link to='/inProgress'>
+          {/* <Link to='/inProgress'>
             <button
               type="button"
               className="inline-flex rounded-md bg-black px-3 py-2 text-sm font-semibold text-white hover:scale-110 hover:bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 items-center hover:text-white transition ease-in-out duration-300"
@@ -29,7 +29,7 @@ function Listing() {
               View All
               <ArrowRight className="ml-2 h-4 w-4" />
             </button>
-          </Link>
+          </Link> */}
         </div>
       </div>
     </div>
